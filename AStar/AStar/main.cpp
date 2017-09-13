587a77cc0404259118497bcece53f0c4f4a00974
Lance Chaney <lance.e.chaney@gmail.com>
2017-09-13 17:58:22 +1200

    Basic Setup for drawing stuff

--- AStar/AStar/main.cpp
+++ AStar/AStar/main.cpp
@@ -1,67 +1,108 @@
-#include <nanogui\nanogui.h>
+#include <iostream>
+#include <nanogui/screen.h>
+#include <nanogui/window.h>
+#include <nanogui/layout.h>
+#include <nanogui/button.h>
+#include <nanogui/glutil.h>
+#include <nanogui/label.h>
+#include <nanogui/theme.h>
+#include <nanogui/formhelper.h>
+#include <nanogui/slider.h>
 
-using namespace nanogui;
+class AStarApp : public nanogui::Screen {
+public:
+	AStarApp() : 
+		m_modulation{ 5 },
+		nanogui::Screen(nanogui::Vector2i(1024, 768), "NanoGUI Test")
+	{
+		using namespace nanogui;
 
-enum test_enum {
-	Item1 = 0,
-	Item2,
-	Item3
-};
+		/**
+		* Add a window.
+		* To the window add a label and a slider widget.
+		*/
 
-bool bvar = true;
-int ivar = 12345678;
-double dvar = 3.1415926;
-float fvar = (float)dvar;
-std::string strval = "A string";
-test_enum enumval = Item2;
-Color colval(0.5f, 0.5f, 0.7f, 1.f);
+		Window* window = new Window(this, "");
+		window->setPosition({ 15, 15 });
+		window->setLayout(new GroupLayout(5, 5, 0, 0));
 
-int main(int /* argc */, char ** /* argv */) {
-	nanogui::init();
+		Label *l = new Label(window, "MODULATION", "sans-bold");
+		l->setFontSize(10);
+		auto slider = new Slider(window);
+		slider->setValue(0.5f);
+		slider->setCallback([this](float value) { m_modulation = value * 10.0f; });
 
-	/* scoped variables */ {
-		bool use_gl_4_1 = false;// Set to true to create an OpenGL 4.1 context.
-		Screen *screen = nullptr;
+		// Do the layout calculations based on what was added to the GUI
+		performLayout();
 
-		if (use_gl_4_1) {
-			// NanoGUI presents many options for you to utilize at your discretion.
-			// See include/nanogui/screen.h for what all of these represent.
-			screen = new Screen(Vector2i(500, 700), "NanoGUI test [GL 4.1]",
-				/*resizable*/true, /*fullscreen*/false, /*colorBits*/8,
-				/*alphaBits*/8, /*depthBits*/24, /*stencilBits*/8,
-				/*nSamples*/0, /*glMajor*/4, /*glMinor*/1);
-		}
-		else {
-			screen = new Screen(Vector2i(500, 700), "NanoGUI test");
-		}
+		// Load GLSL shader code
+		m_shader.initFromFiles("raymarching_shader", "Shaders/vert.glsl", "Shaders/frag.glsl");
 
-		bool enabled = true;
-		FormHelper *gui = new FormHelper(screen);
-		ref<Window> window = gui->addWindow(Eigen::Vector2i(10, 10), "Form helper example");
-		gui->addGroup("Basic types");
-		gui->addVariable("bool", bvar);
-		gui->addVariable("string", strval);
+		/**
+		* Fill the screen with a rectangle (2 triangles)
+		*/
+		MatrixXu indices(3, 2);
+		indices.col(0) << 0, 1, 2;
+		indices.col(1) << 2, 1, 3;
 
-		gui->addGroup("Validating fields");
-		gui->addVariable("int", ivar)->setSpinnable(true);
-		gui->addVariable("float", fvar);
-		gui->addVariable("double", dvar)->setSpinnable(true);
+		MatrixXf positions(3, 4);
+		positions.col(0) << -1, -1, 0;
+		positions.col(1) << 1, -1, 0;
+		positions.col(2) << -1, 1, 0;
+		positions.col(3) << 1, 1, 0;
 
-		gui->addGroup("Complex types");
-		gui->addVariable("Enumeration", enumval, enabled)
-			->setItems({ "Item 1", "Item 2", "Item 3" });
-		gui->addVariable("Color", colval);
+		// bind the shader and upload vertex positions and indices
+		m_shader.bind();
+		m_shader.uploadIndices(indices);
+		m_shader.uploadAttrib("a_position", positions);
 
-		gui->addGroup("Other widgets");
-		gui->addButton("A button", []() { std::cout << "Button pressed." << std::endl; });
+		// Set initial value for modulation uniform
+		m_shader.setUniform("modulation", m_modulation);
 
-		screen->setVisible(true);
-		screen->performLayout();
-		window->center();
-
-		nanogui::mainloop();
+		// Set resolution and screenRatio uniforms
+		int fboWidth, fboHeight;
+		glfwGetFramebufferSize(mGLFWWindow, &fboWidth, &fboHeight);
+		m_shader.setUniform("resolution", Vector2f{ fboWidth, fboHeight });
+		float mx = std::max<float>(fboWidth, fboHeight);
+		auto xDim = fboWidth / mx;
+		auto yDim = fboHeight / mx;
+		m_shader.setUniform("screenRatio", Vector2f{ xDim, yDim });
 	}
 
+	// Draw opengl stuff (screen will handle clear and buffer swap)
+	virtual void drawContents() override 
+	{
+		m_shader.bind();
+		m_shader.setUniform("modulation", m_modulation);
+		m_shader.drawIndexed(GL_TRIANGLES, 0, 2);
+	}
+
+	// Draw nanonvg (vector graphics) stuff
+	virtual void draw(NVGcontext* ctx)
+	{
+		nvgBeginPath(ctx);
+		nvgRect(ctx, 100, 100, 120, 30);
+		nvgCircle(ctx, 120, 120, 5);
+		nvgPathWinding(ctx, NVG_HOLE);	// Mark circle as a hole.
+		nvgFillColor(ctx, nvgRGBA(255, 192, 0, 255));
+		nvgFill(ctx);
+
+		Screen::draw(ctx);
+	}
+
+private:
+	nanogui::GLShader m_shader;
+	float m_modulation;
+};
+
+int main() {
+	nanogui::init();
+
+	AStarApp app;
+	app.setVisible(true);
+
+	nanogui::mainloop();
+
 	nanogui::shutdown();
-	return 0;
+	exit(EXIT_SUCCESS);
 }
